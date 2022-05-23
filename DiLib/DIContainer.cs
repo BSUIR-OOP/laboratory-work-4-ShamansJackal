f628df3a0b40bbf883fccf2f3e621135d9b8499f
p.horovenko <p.horovenko@LINKER-W3>
2022-05-23 14:32:09 +0300

    Di container

---
+++ DiLib/DIContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiLib
+{
+    delegate object GetServiceDelegate();
+
+    public class DIContainer
+    {
+        private Dictionary<Type, GetServiceDelegate> _services = new Dictionary<Type, DiLib.GetServiceDelegate>();
+
+
+        public void AddSingelton<TService>() where TService: class
+        {
+
+        }
+
+        public void AddSingelton<TService, TImplementaion>() where TService: class where TImplementaion: class, TService
+        {
+
+        }
+
+        public void AddTransition<TService>() where TService : class
+        {
+
+        }
+
+        public void AddTransition<TService, TImplementaion>() where TService : class where TImplementaion : class, TService
+        {
+
+        }
+    }
+}
